d1a2d59b2e5768fe95631368fd59e6b282542ba6
bugraatees <bugraatees@hotmail.com>
2024-09-03 15:06:47 +0300

    BeatifulSoup Uygulaması

--- imdb.py
+++ imdb.py
@@ -15,4 +15,4 @@ liste = soup.find("ul", {"class" : "ipc-metadata-list"}).find_all("li", limit=10
 for item in liste:
     filmadi = item.find("h3", {"class":"ipc-title__text"}).text
     puan = item.find("span", {"class":"ipc-rating-star--rating"}).text
-    print(filmadi, puan)
+    print(filmadi, puan) 
